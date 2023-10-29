4a8f38df5d9038850d35c036c700fb44c2d38239
AnonPaperCode <52903562+AnonPaperCode@users.noreply.github.com>
2023-10-29 13:20:50 -0400

    Add files via upload

--- huggins_etal_comparison/viabel_simulations.R
+++ huggins_etal_comparison/viabel_simulations.R
@@ -19,7 +19,7 @@ source('huggins_comparison/viabel_functions.R')
 
 ################################################################################
 # W2L2 varying dimension
-p <- 2 
+p <- 2
 
 alpha <- 0.5
 no_chains <- 20
@@ -31,8 +31,8 @@ for (dimension in seq(2,10,2)){
   # Covariance matrix
   cov_mat <- cov_matrix(alpha, dimension)
   inverse_cov_matrix <- cov_matrix_inv(alpha, dimension)
-  
-  coupled_chain_sampler_mala <- 
+
+  coupled_chain_sampler_mala <-
     function(){
       return(coupled_mala(SamplerQ(1),SamplerP(1), LogPdfQ,LogPdfP,
                           GradLogPdfQ, GradLogPdfP, sigma_mh, chain_length,
@@ -40,10 +40,10 @@ for (dimension in seq(2,10,2)){
     }
   crn_mala <- wp_ub_estimate(coupled_chain_sampler_mala, no_chains=no_chains,
                              p=2, metric=metric_l2, parallel=TRUE)
-  
+
   crn_coupling_ub_mean <- crn_mala$wp_ub
   crn_coupling_ub_sd <- crn_mala$wp_ub_se
-  
+
   # Linear program
   empirical_w2_power_p = rep(NA,no_chains)
   for (i in c(1:no_chains)){
@@ -57,25 +57,25 @@ for (dimension in seq(2,10,2)){
   linear_program_sd = (sd(empirical_w2_power_p)/sqrt(no_chains))^(1/p)
 
   # Only for p=2
-  indep_coupling_mean <- (2*dimension)^(1/p) 
+  indep_coupling_mean <- (2*dimension)^(1/p)
   indep_coupling_sd <- 0
-  
+
   # Only for p=2
   cov_mat_sqrt <- expm::sqrtm(cov_mat)
   true_W2_mean <- norm(cov_mat_sqrt-diag(dimension), type = 'F') # True W2L2 known
   true_W2_sd <- 0
-  
+
   # Huggins bound
   no_samples <- chain_length*2
   unnormalised_target_logpdf <- LogPdfP
-  LogPdfQ_normalized <- 
+  LogPdfQ_normalized <-
     function(x) {
       # dimension <- length(x)
       norm_constant <- -(dimension/2)*log(2*pi)
       return(norm_constant+LogPdfQ(x))
     }
   # NOTE: need proposal_logpdf to be normalized
-  proposal_logpdf <- LogPdfQ_normalized 
+  proposal_logpdf <- LogPdfQ_normalized
   proposal_componentwise_variances <- diag(cov_mat)
   # elbo_proposal_logpdf <- LogPdfQ
   LogPdfP_normalized <-
@@ -85,35 +85,35 @@ for (dimension in seq(2,10,2)){
       return(norm_constant+LogPdfP(x))
     }
   elbo_proposal_logpdf <- LogPdfP_normalized
-  
+
   huggins_W2L2_ubs <- rep(NA, no_chains)
   for(i in c(1:no_chains)){
     proposal_samples <- SamplerQ(no_samples)
     elbo_proposal_samples <- SamplerQ(no_samples)
-    huggins_W2L2_ubs[i] <- 
-      huggins_W2L2_ub_gaussian(unnormalised_target_logpdf, proposal_logpdf, proposal_samples, 
+    huggins_W2L2_ubs[i] <-
+      huggins_W2L2_ub_gaussian(unnormalised_target_logpdf, proposal_logpdf, proposal_samples,
                                proposal_componentwise_variances, elbo_proposal_logpdf, elbo_proposal_samples)
   }
   huggins_mean = mean(huggins_W2L2_ubs^p)^(1/p)
   huggins_sd = (sd(huggins_W2L2_ubs^p)/sqrt(no_chains))^(1/p)
-  
-  trajectory_dimension_df <- 
-    rbind(trajectory_dimension_df, 
+
+  trajectory_dimension_df <-
+    rbind(trajectory_dimension_df,
           data.frame(metric_mean=true_W2_mean, metric_sd=true_W2_sd, type='true_W2L2', dimension=dimension),
           data.frame(metric_mean=crn_coupling_ub_mean, metric_sd=crn_coupling_ub_sd, type='mala_crn', dimension=dimension),
           data.frame(metric_mean=linear_program_mean, metric_sd=linear_program_sd, type='linear_program', dimension=dimension),
           data.frame(metric_mean=indep_coupling_mean, metric_sd=indep_coupling_sd, type='indep', dimension=dimension),
           data.frame(metric_mean=huggins_mean, metric_sd=huggins_sd, type='huggins_W2L2_ub', dimension=dimension))
-  
+
   print(dimension)
 }
 
-mvn_plot_dim <- 
-  ggplot(trajectory_dimension_df %>% filter(type != 'linear_program'), 
-         aes(x=dimension, y=metric_mean, linetype=type)) + 
+mvn_plot_dim <-
+  ggplot(trajectory_dimension_df %>% filter(type != 'linear_program'),
+         aes(x=dimension, y=metric_mean, linetype=type)) +
   geom_line(size=1) + xlab(TeX('Dimension $d$')) + ylab(TeX('$W_2$ upper bounds')) +
   scale_linetype_manual(name=TeX(''), breaks = c('indep','mala_crn','huggins_W2L2_ub','true_W2L2'),
-                        labels=unname(TeX(c('Indep. Coupling','CUB_2', 
+                        labels=unname(TeX(c('Indep. Coupling','CUB_2',
                                             'Huggins et al.', 'True $W_2$'))),
                         values = c('dotted', 'solid', 'dotdash', 'dashed', 'longdash')) +
   geom_ribbon(aes(ymin=metric_mean-metric_sd, ymax=metric_mean+metric_sd), alpha=0.2, colour = NA) +
@@ -121,8 +121,8 @@ mvn_plot_dim <-
   # scale_x_continuous(breaks=seq(0,2e3,200)) +
   # scale_y_continuous(limits = c(1,2100), trans = 'log10') +
   # scale_y_continuous(trans = 'log10') +
-  # scale_y_continuous(breaks=seq(0,,1)) + 
-  theme_classic(base_size = 18) + 
+  # scale_y_continuous(breaks=seq(0,,1)) +
+  theme_classic(base_size = 18) +
   theme(legend.key.width=unit(1.25,"cm"))
   # guides(linetype=guide_legend(nrow=2,byrow=TRUE))
 mvn_plot_dim
